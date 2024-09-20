f9e5e634960e18bcc9c4dea9675d576d0b63073f
v_jpch <v_jpch.digitalgd.com>
2024-09-20 09:50:58 +0800

    chore: 调整常量

--- app/const.js
+++ app/const.js
@@ -3,7 +3,17 @@
 const SUCCESS = 0;
 const FAILED = -1;
 
+// 白名单命令，不在此白名单中的不给执行，防止用户插入风险操作，如：rm -rf等
+const COMMAND_WHITELIST = [
+  'npm', // Node.js的包管理工具
+  'cnpm', // npm的中国镜像加速版
+  'yarn', // 另一个流行的JavaScript包管理工具
+  'pnpm', // 性能更优的npm替代品
+  'node', // Node.js运行环境
+];
+
 module.exports = {
   SUCCESS,
   FAILED,
+  COMMAND_WHITELIST,
 };
--- app/models/CloudBuildTask.js
+++ app/models/CloudBuildTask.js
@@ -13,22 +13,13 @@ const path = require('path');
 const OSS = require('./OSS');
 const config = require('../../config/db');
 const helper = require('../extend/helper');
-const { SUCCESS, FAILED } = require('../const');
+const { SUCCESS, FAILED, COMMAND_WHITELIST } = require('../const');
 
 const DEFAULT_CLI_HOME = '.cjp-cli-dev'; // 默认缓存路径
 const CLOUD_BUILD_DIR = 'cloudbuild'; // 默认缓存路径
 const TAOBAO_REGISTRY = 'https://registry.npmmirror.com/'; // 淘宝源
 
 const REDIS_PREFIX = 'cloudbuild';
-
-// 白名单命令，不在此白名单中的命令都需要确认是否执行，防止用户插入风险操作，如：rm -rf等
-const COMMAND_WHITELIST = [
-  'npm', // Node.js的包管理工具
-  'cnpm', // npm的中国镜像加速版
-  'yarn', // 另一个流行的JavaScript包管理工具
-  'pnpm', // 性能更优的npm替代品
-  'node', // Node.js运行环境
-];
 
 class CloudBuildTask {
   constructor(options, ctx, app) {
